029ea77443f64cedb936b1f192937e3316f40caa
Mark Lester Bolotaolo <mlab817@gmail.com>
2023-04-14 13:50:39 +0800

    fixed error in package.json

--- dist/assets/new-button.component.1dfad5da.js
+++
@@ -1,1 +0,0 @@
-import{ak as o,b as r,B as n,j as p,an as t}from"./vendor.876bea49.js";import{u as a}from"./index.c1131783.js";const i=({onClick:s})=>{const{submissionOpen:e}=a();return r(n,{type:"primary",onClick:s,disabled:!e,children:[p(t,{}),"New"]})};i.propTypes={onClick:o.exports.func};export{i as N};
